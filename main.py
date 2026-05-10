6681b6f02b71a27802fddf487ea5342f0e7579ae
abdomo4112005-blip <abdomo4112005@gmail.com>
2026-05-11 02:34:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 import requests
 import asyncio
 from telegram import Bot
+from telegram.request import HTTPXRequest
 
 # ========== إعداداتك ==========
-TOKEN = "8446317533:AAGjXlloTojDWnk8IPreIwvjCuNl8j4o2MY"
+TOKEN = "8446317533:AAGjXl1oTojDWnk8IPreIwvjCFq7tM28pM8"
 CHAT_ID = "@rn_h_i"
-INTERVAL = 1 
+INTERVAL = 60 
 
-bot = Bot(token=TOKEN)
-
-def get_ton_price():
-    try:
-        url = "https://api.binance.com/api/v3/ticker/price?symbol=TONUSDT"
-        data = requests.get(url).json()
-        return float(data["price"])
-    except:
-        return None
-
-async def send_price():
-    print("بدأ البوت في إرسال التحديثات...")
+# نظام اتصال أقوى عشان يتخطى مشاكل السيرفرات
+async def start_bot():
+    t_request = HTTPXRequest(connection_pool_size=8)
+    bot = Bot(token=TOKEN, request=t_request)
+    
+    print("بدء محاولة الاتصال بالAPI...")
+    
     while True:
-        price_usd = get_ton_price()
-        if price_usd is not None:
-            message = f"{price_usd:.2f}$"
-            try:
-                await bot.send_message(chat_id=CHAT_ID, text=message)
-            except Exception as e:
-                print(f"خطأ: {e}")
+        try:
+            # جلب السعر من بينانس
+            url = "https://api.binance.com/api/v3/ticker/price?symbol=TONUSDT"
+            response = requests.get(url)
+            data = response.json()
+            price = float(data["price"])
+            
+            # إرسال الرسالة
+            text = f"💎 سعر عملة TON الآن:\n\n💰 ${price:.2f}"
+            await bot.send_message(chat_id=CHAT_ID, text=text)
+            print(f"✅ تم الإرسال للقناة: {price}")
+            
+        except Exception as e:
+            print(f"❌ حدث خطأ: {e}")
+            
         await asyncio.sleep(INTERVAL)
 
-async def main():
-    await send_price()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_bot())
